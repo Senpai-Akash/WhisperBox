8a9d6717aa0eaf246b6bb24a753b0670a95dd122
aayush <aayush.devvv@gmail.com>
2025-11-30 10:53:49 +0530

    Day-01 Navbar
    Created Navbar and Worked on functionality.

---
+++ cbp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './components/navbar.jsx'
+import Body from './components/body.jsx'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Navbar/>
+      <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ cbp/src/components/navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+      <nav className="bg-gradient-to-l from-black/60 via-purple-700/50 to-purple-500/40 p-4 m-5 rounded-full">
+
+        <div className="flex justify-between items-center">
+          <div className="logo">
+
+          <ul>
+            <li className='font-bold'>
+              {/* split WHISPER into per-letter spans so we can animate letters individually */}
+              <span className="text-4xl whisper whisper--play">
+                {'WHISPER'.split('').map((ch, i) => (
+                  <span
+                    key={i}
+                    className="whisper-letter inline-block"
+                    style={{ ['--i']: i }}
+                  >
+                    {ch}
+                  </span>
+                ))}
+              </span>
+
+              <span className='text-white text-2xl ml-2'>BOX</span>
+            </li>
+          </ul>
+          </div>
+
+          <div className="info">
+            <ul className="flex space-x-6 text-white">
+              <li className="relative group cursor-pointer">
+    Home
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-purple-400 group-hover:w-full transition-all duration-300"></span>
+  </li>
+  <li className="relative group cursor-pointer">
+    About
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-purple-400 group-hover:w-full transition-all duration-300"></span>
+  </li>
+  <li className="relative group cursor-pointer">
+    Contact
+    <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-purple-400 group-hover:w-full transition-all duration-300"></span>
+  </li>
+</ul>
+
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default navbar
